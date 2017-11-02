4b13ca565e6d875d02dbd78cacac877746217ddd
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-02 15:09:32 -0400

    created cocktails controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,14 +1,23 @@
 class CocktailsController < ApplicationController
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(find_cocktail)
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +27,15 @@ class CocktailsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
   end
 end
