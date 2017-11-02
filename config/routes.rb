1e72f61ab6c7a1c10cb23fbba72c7ac84448a763
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-02 14:34:28 -0400

    Validated models for cocktail, ingredient and doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'cocktail/index'
+
+  get 'cocktail/show'
+
+  get 'cocktail/new'
+
+  get 'cocktail/create'
+
+  get 'cocktail/edit'
+
+  get 'cocktail/update'
+
+  get 'cocktail/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20171102181905_create_doses.rb
+++ db/migrate/20171102181905_create_doses.rb
@@ -1,9 +1,9 @@
 class CreateDoses < ActiveRecord::Migration[5.0]
   def change
     create_table :doses do |t|
-      t.references :cocktails, foreign_key: true
-      t.references :ingredients, foreign_key: true
       t.text :description
+      t.references :cocktail, foreign_key: true
+      t.references :ingredient, foreign_key: true
 
       t.timestamps
     end
