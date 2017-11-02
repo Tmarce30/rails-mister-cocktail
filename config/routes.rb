d35f4cb0bada71ab4a7058a707e0769510063877
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-02 14:56:52 -0400

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
-  get 'cocktail/index'
-
-  get 'cocktail/show'
-
-  get 'cocktail/new'
-
-  get 'cocktail/create'
-
-  get 'cocktail/edit'
-
-  get 'cocktail/update'
-
-  get 'cocktail/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :doses
+  end
+
+  delete 'doses/:id', to: 'doses#destroy'
 end
